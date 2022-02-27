53760e36e55be50748784682daa875f9ab556c85
Vladimir Rujević <vladimir.rujevic@live.com>
2022-02-27 11:45:17 +0100

    stage 4

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,4 +1,11 @@
 # write your code here
+def is_number(x):
+    try:
+        int(x)
+    except ValueError:
+        return False
+    return True
+
 
 def main():
     terms = None
@@ -8,13 +15,24 @@ def main():
         if terms == '/exit':
             continue
         elif terms == '/help':
-            print('The program calculates the sum of numbers')
+            print('The program is a simple calculator, terms and operations are separated by spaces.')
             continue
         elif terms == '':
             continue
-
-        terms = [int(x) for x in terms.split()]
-        print(sum(terms))
+        factor = 1
+        result = 0
+        terms = terms.split()
+        for term in terms:
+            if '-' in term and not is_number(term):
+                if len(term) % 2 == 0 and '+' not in term:
+                    factor = 1
+                else:
+                    factor = -1
+            elif '+' in term:
+                factor = 1
+            else:
+                result += factor * int(term)
+        print(result)
 
     print('Bye!')
 
