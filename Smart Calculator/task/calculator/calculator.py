3383f10b4be288dfc21e69c3987a10424e760d31
Vladimir Rujević <vladimir.rujevic@live.com>
2022-04-17 22:37:22 +0200

    stage 7 - final

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,6 +1,4 @@
 from calc import Calc, is_number
-from collections import deque
-import re
 
 
 def menu(command):
@@ -32,8 +30,6 @@ def main():
             if r is not True:
                 print(r)
         else:
-            factor = 1
-            result = 0
             expression = calculator.infix_to_postfix(expression)
             if not expression:
                 print('Invalid expression')
@@ -46,34 +42,6 @@ def main():
                     print('Invalid expression')
                 else:
                     print(result)
-                """
-                for index, term in enumerate(expression):
-                    if index % 2 == 0:
-                        if is_number(term):
-                            result += factor * int(term)
-                        elif term.isalnum():
-                            n = calculator.get_value(term)
-                            if not is_number(n):
-                                print(n)
-                                result = False
-                                break
-                            else:
-                                result += factor * int(n)
-                        else:
-                            print('Invalid expression')
-                            result = False
-                            break
-                    else:
-                        if '-' in term and not is_number(term):
-                            if len(term) % 2 == 0 and '+' not in term:
-                                factor = 1
-                            else:
-                                factor = -1
-                        elif '+' in term:
-                            factor = 1
-                if result is not False:
-                    print(result)
-                """
 
     print('Bye!')
 
